4ce418df851d20f2a74739b5f8823270afb6a7e4
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-03 13:53:28 +0100

    Update bun.js

--- greieyn/creshoh/bun.js
+++ greieyn/creshoh/bun.js
@@ -13,14 +13,14 @@ const earrooFeyshtyn = Object.keys(focklynGamman).length * 4;
         2 * earrooFeyshtyn,
         1 * earrooFeyshtyn
     ]);
+    trogAyrn({paarant: true, red: "div", ennym: "saagh-toshiaght", cheuSthie: [
+        trogAyrn({red: "div", ennym: "coyrle-toshiaght", cheuSthie: [
+            "Click the word that matches the picture!",
+            trogAyrn({red: "button", ennym: "cramman-toshiaght", cheuSthie: ["ok"], keint: ["reih"], click: t => gowToshiaght(t, "creshoh")}),
+            trogAyrn({red: "button", ennym: "cramman-faagail", cheuSthie: ["exit game"], keint: ["reih", "cramman-faagail"], click: t => caghlaaDuillag(t, "../../../index.html")})
+        ]})
+    ], leayrid: "visible"});
     trogAyrn({paarant: true, red: "div", ennym: "saagh", cheuSthie: [
-        trogAyrn({red: "div", ennym: "saagh-toshiaght", cheuSthie: [
-            trogAyrn({red: "div", ennym: "coyrle-toshiaght", cheuSthie: [
-                "Click the word that matches the picture!",
-                trogAyrn({red: "button", ennym: "cramman-coyrle", cheuSthie: ["ok"], keint: ["reih"], click: t => gowToshiaght(t, "creshoh", true)}),
-                trogAyrn({red: "button", ennym: "cramman-faagail", cheuSthie: ["exit game"], keint: ["reih", "cramman-faagail"], click: t => caghlaaDuillag(t, "../../../index.html")})
-            ]})
-        ], leayrid: "visible"}),
         trogAyrn({red: "div", ennym: "saagh-stoo-gamman", cheuSthie: [
             trogAyrn({red: "div", ennym: "saagh-mullagh", cheuSthie: [
                 trogAyrn({red: "div", ennym: "clag", cheuSthie: [
@@ -37,13 +37,13 @@ const earrooFeyshtyn = Object.keys(focklynGamman).length * 4;
                 trogAyrn({red: "div", ennym: "saagh-caslys", cheuSthie: [
                     trogAyrn({red: "img", ennym: "caslys", cheuSthie: focklynGamman[reihyn[0]].caslys})
                 ]}),
-                trogAyrn({red: "button", ennym: "reih-1", cheuSthie: [crammanyn[0]], keint: ["reih"], click: clickit}),
-                trogAyrn({red: "button", ennym: "reih-2", cheuSthie: [crammanyn[1]], keint: ["reih"], click: clickit}),
-                trogAyrn({red: "button", ennym: "reih-3", cheuSthie: [crammanyn[2]], keint: ["reih"], click: clickit})
-            ]}),
-            trogAyrn({red: "div", ennym: "saagh-dooney", cheuSthie: [
-                trogAyrn({red: "button", ennym: "cramman-dooney", cheuSthie: ["exit game"], click: t => caghlaaDuillag(t, "../../../index.html")})
+                trogAyrn({red: "button", ennym: "reih-1", cheuSthie: [crammanyn[0]], keint: ["reih-creshoh"], click: clickit}),
+                trogAyrn({red: "button", ennym: "reih-2", cheuSthie: [crammanyn[1]], keint: ["reih-creshoh"], click: clickit}),
+                trogAyrn({red: "button", ennym: "reih-3", cheuSthie: [crammanyn[2]], keint: ["reih-creshoh"], click: clickit})
             ]})
+        ]}),
+        trogAyrn({red: "div", ennym: "saagh-dooney", cheuSthie: [
+            trogAyrn({red: "button", ennym: "cramman-dooney", cheuSthie: ["exit game"], click: t => caghlaaDuillag(t, "../../../index.html")})
         ]})
     ]});
 })();
