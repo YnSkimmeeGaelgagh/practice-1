1500d3cf521909025909633bfd586123d467171a
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-03 14:38:11 +0100

    Update feyshteyder.js

--- greieyn/screeu/feyshteyder.js
+++ greieyn/screeu/feyshteyder.js
@@ -3,7 +3,7 @@ function focklyn () {
     saaghFocklyn.textContent = "";
     let focklyn = raaghyn[earrooFeysht].gaelg.split(" ");
     const caslys = document.getElementById("caslys");
-    caslys.style.visibility = hidden;
+    caslys.style.visibility = "hidden";
     caslys.classList.remove("kiart");
     const rollageyn = [...document.getElementsByClassName("saagh-rollage")];
     rollageyn.forEach(r => r.remove());
