735e2cb21cafcba2b535e8ad114334dbc11d03fd
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-04 13:11:55 +0100

    Update focklyn.js

--- lessoonyn/bee/fysseree/focklyn.js
+++ lessoonyn/bee/fysseree/focklyn.js
@@ -1,38 +1,38 @@
 const focklynGamman = {
     "ooylyn": {
         caslys: "../caslyssyn/ooylyn.webp",
-        sheean: "sheeanyn/ooylyn.mp3"
+        sheean: "../sheeanyn/ooylyn.mp3"
     },
     "bagoon": {
         caslys: "../caslyssyn/bagoon.webp",
-        sheean: "sheeanyn/bagoon.mp3"
+        sheean: "../sheeanyn/bagoon.mp3"
     },
     "arran": {
         caslys: "../caslyssyn/arran.webp",
-        sheean: "sheeanyn/arran.mp3"
+        sheean: "../sheeanyn/arran.mp3"
     },
     "caashey": {
         caslys: "../caslyssyn/caashey.webp",
-        sheean: "sheeanyn/caashey.mp3"
+        sheean: "../sheeanyn/caashey.mp3"
     },
     "spollagyn": {
         caslys: "../caslyssyn/spollagyn.webp",
-        sheean: "sheeanyn/spollagyn.mp3"
+        sheean: "../sheeanyn/spollagyn.mp3"
     },
     "eeast": {
         caslys: "../caslyssyn/eeast.webp",
-        sheean: "sheeanyn/eeast.mp3"
+        sheean: "../sheeanyn/eeast.mp3"
     },
     "noiridyn": {
         caslys: "../caslyssyn/noiridyn.webp",
-        sheean: "sheeanyn/noiridyn.mp3"
+        sheean: "../sheeanyn/noiridyn.mp3"
     },
     "pronnagyn": {
         caslys: "../caslyssyn/pronnagyn.webp",
-        sheean: "sheeanyn/pronnagyn.mp3"
+        sheean: "../sheeanyn/pronnagyn.mp3"
     },
     "lossreeyn": {
         caslys: "../caslyssyn/lossreeyn.webp",
-        sheean: "sheeanyn/lossreeyn.mp3"
+        sheean: "../sheeanyn/lossreeyn.mp3"
     }
 };
