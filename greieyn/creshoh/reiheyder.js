7dc31c2fedec94f62c9614464084c99bfc79df12
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-06 14:05:36 +0100

    Update reiheyder.js

--- greieyn/creshoh/reiheyder.js
+++ greieyn/creshoh/reiheyder.js
@@ -3,8 +3,8 @@ let reihyn = [];
 function soieReihyn () {
     reihyn = [];
     let nyFocklyn = [];
-    for (let f of Object.keys(focklynGamman)) nyFocklyn.push(f);
-    let j = focklynGamman.size - 1;
+    for (let f of Object.keys(raaghyn)) nyFocklyn.push(f);
+    let j = raaghyn.size - 1;
     while (nyFocklyn.length) {
         let r = Math.floor(Math.random() * nyFocklyn.length);
         reihyn.push(nyFocklyn[r]);
