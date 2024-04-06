527bbecbf75093037211b3cafd2103bdb97ec263
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-06 14:06:20 +0100

    Update bunJerrey.js

--- greieyn/bunJerrey.js
+++ greieyn/bunJerrey.js
@@ -1,33 +1,46 @@
-function troggJerrey (aggDeanyn) {
-    trogAyrn({leayrid: "hidden", paarant: true, red: "div", ennym: "saagh-jeant", cheuSthie: [
-        trogAyrn({red: "h2", ennym: "teks-agg-jerrinagh", cheuSthie: ["Final Score"]}),
-        trogAyrn({red: "div", ennym: "agg-jerrinagh"}),
-        trogAyrn({red: "div", ennym: "moylley", cheuSthie: ["Jeant dy mie!"]}),
-        trogAyrn({red: "div", cheuSthie: [
-            trogAyrn({red: "button", ennym: "cramman-reesht", cheuSthie: ["again"], click: t => caghlaaDuillag(t, location.pathname)}),
-            trogAyrn({red: "button", ennym: "cramman-jeant", cheuSthie: ["finish"], click: t => caghlaaDuillag(t,  "../../../index.html")})
-        ]}),
-        trogAyrn({red: "div", ennym: 'boayrd-agg', cheuSthie: [
-            trogAyrn({red: "div", ennym: "saagh-yindyssagh", cheuSthie: [
-                trogAyrn({red: "div", ennym: "yindyssagh", cheuSthie: ["yindyssagh"], keint: ["agg-ennym"]}),
-                trogAyrn({red: "div", ennym: "agg-yindyssagh", cheuSthie: [aggDeanyn[0]]})
-            ], keint: ["aggyn"]}),
-            trogAyrn({red: "div", ennym: "saagh-niartal", cheuSthie: [
-                trogAyrn({red: "div", ennym: "niartal", cheuSthie: ["niartal"], keint: ["agg-ennym"]}), 
-                trogAyrn({red: "div", ennym: "agg-niartal", cheuSthie: [aggDeanyn[1]]})
-            ], keint: ["aggyn"]}),
-            trogAyrn({red: "div", ennym: "saagh-feer-vie", cheuSthie: [
-                trogAyrn({red: "div", ennym: "feer-vie", cheuSthie: ["feer vie"], keint: ["agg-ennym"]}),
-                trogAyrn({red: "div", ennym: "agg-feer-vie", cheuSthie: [aggDeanyn[2]]})
-            ], keint: ["aggyn"]}),
-            trogAyrn({red: "div", ennym: "saagh-braew", cheuSthie: [
-                trogAyrn({red: "div", ennym: "braew", cheuSthie: ["braew"], keint: ["agg-ennym"]}),
-                trogAyrn({red: "div", ennym: "agg-braew", cheuSthie: [aggDeanyn[3]]})
-            ], keint: ["aggyn"]}),
-            trogAyrn({red: "div", ennym: "saagh-mdl", cheuSthie: [
-                trogAyrn({red: "div", ennym: "mdl", cheuSthie: ["mie dy liooar"], keint: ["agg-ennym"]}), 
-                trogAyrn({red: "div", ennym: "agg-mdl", cheuSthie: [aggDeanyn[4]]})
-            ], keint: ["aggyn"]})
+const earrooFeyshtyn = Object.keys(raaghyn);
+const mooadys = earrooFeyshtyn.length;
+
+function troggDuillag () {
+    if (earrooFeysht == 1) {
+        prowTrooidGamman();
+        trogAyrn({paarant: true, red: "div", ennym: "saagh-toshiaght", cheuSthie: [
+            trogAyrn({red: "div", ennym: "coyrle-toshiaght", cheuSthie: [
+                "Click the word blocks to complete the sentences!",
+                trogAyrn({red: "button", ennym: "cramman-toshiaght", cheuSthie: ["ok"], keint: ["reih"], click: t => gowToshiaght(t, "screeu")}),
+                trogAyrn({red: "button", ennym: "cramman-faagail", cheuSthie: ["exit game"], keint: ["reih", "cramman-faagail"], click: t => caghlaaDuillag(t, "../../../index.html")})
+            ]})
+        ], leayrid: "visible"});
+        let ayrn = 1;
+        const deanyn = [];
+        for (let i = 0; i < 5; i++) {
+            const earroo = Math.round(mooadys * ayrn);
+            deanyn.push(earroo);
+            ayrn -= .2;
+        }
+        troggJerrey(deanyn);
+        trogAyrn({paarant: true, red: "div", ennym: "saagh", cheuSthie: [
+            trogAyrn({red: "div", ennym: "cleeah", cheuSthie: [
+                trogAyrn({red: "div", ennym: "saagh-greinnaghey", cheuSthie: [
+                    trogAyrn({red: "div", ennym: "saagh-caslys", cheuSthie: [
+                        trogAyrn({red: "img", ennym: "caslys"})
+                    ]}),
+                    trogAyrn({red: "div", ennym: "greinnaghey"})
+                ]}),
+                trogAyrn({red: "div", ennym: "saagh-freggyrt"}),
+                trogAyrn({red: "div", ennym: "saagh-cramman", cheuSthie: [
+                    trogAyrn({red: "button", ennym: "cramman-freggyrt", cheuSthie: ["check"]})
+                ]}),
+                trogAyrn({red: "div", ennym: "saagh-focklyn"})
+            ]}),
+            trogAyrn({red: "div", ennym: "saagh-dooney", cheuSthie: [
+                trogAyrn({red: "button", ennym: "cramman-dooney", cheuSthie: ["exit game"], click: t => caghlaaDuillag(t, "../../../index.html")})
+            ]})
         ]})
-    ]})
-}
+    };
+    soieSaaghFreggyrt();
+    focklyn();
+    scugh();
+};
+
+troggDuillag();
