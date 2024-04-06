011ea6138b83f9aac31d79386ea7107dfba93a25
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-06 14:02:59 +0100

    Update scugheyder.js

--- greieyn/eaishtagh/scugheyder.js
+++ greieyn/eaishtagh/scugheyder.js
@@ -7,8 +7,6 @@ function scugh () {
     const focklyn = [...document.getElementsByClassName("fockle")];
     const sheeanClick = new Audio("../../../sheeanyn/click.mp3");
     const sheeanArraghey = new Audio("../../../sheeanyn/arraghey.mp3");
-    const kiart = new Audio("../../../sheeanyn/clinck.mp3");
-    const neuChiart = new Audio("../../../sheeanyn/neu-chiart.mp3");
     let pointRyGheddyn = true;
     focklyn.forEach(w => w.onpointerdown = greim);
     function curFockleStiagh (f) {
