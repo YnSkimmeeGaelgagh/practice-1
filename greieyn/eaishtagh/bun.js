5577af7dc2209fb2d6b4d13338ad1a30c06996cc
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-09 12:04:53 +0100

    Update bun.js

--- greieyn/eaishtagh/bun.js
+++ greieyn/eaishtagh/bun.js
@@ -2,7 +2,6 @@ const earrooFeyshtyn = Object.keys(raaghyn);
 const mooadys = earrooFeyshtyn.length;
 
 function troggDuillag () {
-    window.scrollTo(0, 0);
     if (earrooFeysht == 1) {
         prowTrooidGamman();
         trogAyrn({paarant: true, red: "div", ennym: "saagh-toshiaght", cheuSthie: [
@@ -50,6 +49,7 @@ function troggDuillag () {
         ]});
     }
     else eaishtReesht();
+    sfxGoll = true;
     soieSaaghFreggyrt();
     focklyn();
     scugh();
