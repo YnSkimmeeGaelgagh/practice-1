78cb801d6c4f75fa23f2d5ab62c3be4cc00aa515
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-09 12:16:50 +0100

    Update caghlaaghynGamman.js

--- greieyn/caghlaaghynGamman.js
+++ greieyn/caghlaaghynGamman.js
@@ -10,12 +10,12 @@ let kiart;
 let neuChiart;
 let jerreyMie;
 let jerreyCadjin;
-if (veih.endsWith("/index.html") && sfxGoll) {
+if (veih.endsWith("/index.html")) {
     foshley = new Audio("sheeanyn/foshley.mp3");
     dooney = new Audio("sheeanyn/dooney.mp3");
     ymHeidey = new Audio("sheeanyn/ym-heidey.mp3");
     glassBrishey = new Audio("sheeanyn/glass-brishey.mp3");
-} else if (!veih.endsWith("/index.html") && sfxGoll || veih.endsWith("eaishtagh.html")) {
+} else if (!veih.endsWith("/index.html")) {
     kiart = new Audio("../../../sheeanyn/clinck.mp3");
     neuChiart = new Audio("../../../sheeanyn/neu-chiart.mp3");
     jerreyMie = new Audio("../../../sheeanyn/jerrey-mie.mp3");
