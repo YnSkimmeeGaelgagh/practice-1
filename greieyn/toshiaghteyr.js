fffa3efb52fdd58b08efea8f72882c421b6745c4
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-11 11:51:29 +0100

    Update toshiaghteyr.js

--- greieyn/toshiaghteyr.js
+++ greieyn/toshiaghteyr.js
@@ -1,11 +1,8 @@
 function gowToshiaght (t, keintGamman) {
     if (kiaullGoll) lhoob.play();
+    document.body.style.overflow = "scroll";
     if (keintGamman == "creshoh") traa();
-    if (keintGamman == "eaishtagh") {
-        sessionStorage.setItem("sfxGoll", "true");
-        sfxGoll = true;
-        setTimeout(eaishtReesht, 500);
-    };
+    if (keintGamman == "eaishtagh") setTimeout(eaishtReesht, 500);
     saaghToshiaght = document.getElementById("saagh-toshiaght");
     saaghToshiaght.remove();
 };
