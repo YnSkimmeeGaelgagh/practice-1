5e9c017dcd4b5a6af6adde4b6b262def89b32803
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-12 02:39:11 +0100

    Update saaghFreggyrt.js

--- greieyn/eaishtagh/saaghFreggyrt.js
+++ greieyn/eaishtagh/saaghFreggyrt.js
@@ -3,5 +3,6 @@ function soieSaaghFreggyrt () {
     saaghFreggyrt.style.display = "flex";
     saaghFreggyrt.textContent = "Listen to the recording then click the blocks below to write what you hear!";
     saaghFreggyrt.style.color = "grey";
-    saaghFreggyrt.style.textAlign = "center";
+    saaghFreggyrt.style.justifyContent = "center";
+    saaghFreggyrt.style.alignItems = "center";
 }
