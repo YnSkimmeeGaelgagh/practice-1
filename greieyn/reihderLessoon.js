356fa94262a99edd466963d76f5cda58962b97c3
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-15 19:32:13 +0100

    Update reihderLessoon.js

--- greieyn/reihderLessoon.js
+++ greieyn/reihderLessoon.js
@@ -7,6 +7,8 @@ cleeahyn.forEach(c => {
         p.onclick = t => soieLessoon(t, p.title, jalloo, p.textContent.trim());
     });
 });
+const cons = document.getElementById("cons");
+foshley.oncanplaythrough = () => cons.remove();
 mooie(navigator.userAgent);
 function soieLessoon (t, cooish, jalloo, ennym) {
     mooie("toshiaght");
