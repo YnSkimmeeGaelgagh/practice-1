99d0e522e6793d675b5f8519f49d47f24a34991d
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-16 17:32:22 +0100

    Update laadeyder.js

--- greieyn/laadeyder.js
+++ greieyn/laadeyder.js
@@ -13,3 +13,18 @@
     screenLaadey = document.getElementById("screen-laadey");
     document.body.onload = () => screenLaadey.remove();
 })();
+
+function cloieSheean (s) {
+    const context = new AudioContext() || new webkitAudioContext();
+    const request = new XMLHttpRequest();
+    request.open("GET", s, true);
+    request.responseType = "arraybuffer";
+    request.onload = () => context.decodeAudioData(request.response, onDecoded);
+    function onDecoded(buffer) {
+        const bufferSource = context.createBufferSource();
+        bufferSource.buffer = buffer;
+        bufferSource.connect(context.destination);
+        bufferSource.start();
+    };
+    request.send();
+};
