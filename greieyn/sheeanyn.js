a687631a0e000be461cd6b3075178e4ce50383d4
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-16 17:29:06 +0100

    Update sheeanyn.js

--- greieyn/sheeanyn.js
+++ greieyn/sheeanyn.js
@@ -12,3 +12,18 @@ ardSkirreyderSFX.onchange = () => {
     sfxGoll = sessionStorage.getItem("sfxGoll") == "true" ? true : false;
 }
 const caghlaaArdSkirreyderSFX = (stayd) => ardSkirreyderSFX.checked = stayd;
+
+function cloieSheean (s) {
+    const context = new AudioContext() || new webkitAudioContext();
+    const request = new XMLHttpRequest();
+    request.open("GET", s, true);
+    request.responseType = "arraybuffer";
+    request.onload = () => context.decodeAudioData(request.response, onDecoded);
+    function onDecoded(buffer) {
+        const bufferSource = context.createBufferSource();
+        bufferSource.buffer = buffer;
+        bufferSource.connect(context.destination);
+        bufferSource.start();
+    };
+    request.send();
+};
