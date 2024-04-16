72eb15a5f585a0930673ee81880896964f9ee0c4
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-16 17:24:44 +0100

    Update caghlaaghynGamman.js

--- greieyn/caghlaaghynGamman.js
+++ greieyn/caghlaaghynGamman.js
@@ -2,27 +2,28 @@ let kiaullGoll = sessionStorage.getItem("kiaullGoll") == "true" ? true : false;
 let sfxGoll = sessionStorage.getItem("sfxGoll") == "true" ? true : false;
 const veih = location.pathname;
 const foshley = "sheeanyn/foshley.mp3";
-let dooney = "sheeanyn/dooney.mp3";
-let ymHeidey = "sheeanyn/ym-heidey.mp3";
-let glassBrishey = "sheeanyn/glass-brishey.mp3";
-let fockleKiart;
-let kiart;
-let neuChiart;
-let jerreyMie;
-let jerreyCadjin;
+const dooney = "sheeanyn/dooney.mp3";
+const ymHeidey = "sheeanyn/ym-heidey.mp3";
+const glassBrishey = "sheeanyn/glass-brishey.mp3";
+const sheeanAahoit = "sheeanyn/aahoit.mp3";
+const fockleKiart = "../../../sheeanyn/fockle-kiart.mp3";
+const kiart = "../../../sheeanyn/clinck.mp3";
+const neuChiart = "../../../sheeanyn/neu-chiart.mp3";
+const jerreyMie = "../../../sheeanyn/jerrey-mie.mp3";
+const jerreyCadjin = "../../../sheeanyn/jerrey-cadjin.mp3";
 
-if (veih.endsWith("/index.html") || veih.endsWith("/")) {
+// if (veih.endsWith("/index.html") || veih.endsWith("/")) {
     // foshley = document.getElementById("sheean-foshley");
     // dooney = document.getElementById("sheean-dooney");
     // ymHeidey = document.getElementById("sheean-ym-heidey");
     // glassBrishey = document.getElementById("glass-brishey");
-} else if (!veih.endsWith("/index.html")) {
-    fockleKiart = new Audio("../../../sheeanyn/fockle-kiart.mp3");
-    kiart = new Audio("../../../sheeanyn/clinck.mp3");
-    neuChiart = new Audio("../../../sheeanyn/neu-chiart.mp3");
-    jerreyMie = new Audio("../../../sheeanyn/jerrey-mie.mp3");
-    jerreyCadjin = new Audio("../../../sheeanyn/jerrey-cadjin.mp3");
-}
+// } else if (!veih.endsWith("/index.html")) {
+    // fockleKiart = new Audio("../../../sheeanyn/fockle-kiart.mp3");
+    // kiart = new Audio("../../../sheeanyn/clinck.mp3");
+    // neuChiart = new Audio("../../../sheeanyn/neu-chiart.mp3");
+    // jerreyMie = new Audio("../../../sheeanyn/jerrey-mie.mp3");
+    // jerreyCadjin = new Audio("../../../sheeanyn/jerrey-cadjin.mp3");
+// }
 let earrooFeysht = 1;
 let agg = 0;
 
