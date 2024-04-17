c4527eefa7ef546ee53ec5cacbb8f47319f98df8
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-17 17:28:42 +0100

    Update aaEaishteyder.js

--- greieyn/eaishtagh/aaEaishteyder.js
+++ greieyn/eaishtagh/aaEaishteyder.js
@@ -1,10 +1,5 @@
 function eaishtReesht () {
     const caslysEaishtagh = document.getElementById("caslys-eaishtagh");
     caslysEaishtagh.classList.add("sheean");
-    setTimeout(() => {
-        setTimeout(() => {
-            caslysEaishtagh.classList.remove("sheean");
-        }, raaghyn[earrooFeysht].sheean.duration * 1000);
-    }, 100);
-    cloieSheean(raaghyn[earrooFeysht].sheean);
+    cloieSheean({raad: raaghyn[earrooFeysht].sheean, traa: 0.1, caslys: caslysEaishtagh});
 }
