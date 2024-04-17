79621cfbda9b4474da54e34d86b09f840c5d0fd3
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-17 17:28:58 +0100

    Update laadeyder.js

--- greieyn/laadeyder.js
+++ greieyn/laadeyder.js
@@ -7,8 +7,6 @@
             c.src = raaghyn[raa].caslys;
             raaghyn[raa].caslys = c;
         };
-        const s = new Audio(raaghyn[raa].sheean);
-        raaghyn[raa].sheean = s;
     };
     screenLaadey = document.getElementById("screen-laadey");
     document.body.onload = () => screenLaadey.remove();
@@ -17,14 +15,23 @@
 function cloieSheean (s) {
     const context = new AudioContext() || new webkitAudioContext();
     const request = new XMLHttpRequest();
-    request.open("GET", s, true);
+    request.open("GET", s.raad, true);
     request.responseType = "arraybuffer";
     request.onload = () => context.decodeAudioData(request.response, onDecoded);
     function onDecoded(buffer) {
         const bufferSource = context.createBufferSource();
         bufferSource.buffer = buffer;
         bufferSource.connect(context.destination);
-        bufferSource.start();
+        bufferSource.start(s.traa);
+        if (s.caslys) {
+            let mayrnaght = bufferSource.buffer.duration;
+            setTimeout(() => {
+                s.caslys.classList.remove("sheean");
+            }, mayrnaght * 1000);
+        };
+        if (s.screeu) {
+            bufferSource.onended = () => curSaaghBack(true);
+        }
     };
     request.send();
 };
