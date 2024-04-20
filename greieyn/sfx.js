d4d58b567b077fd911ba68f5625b92fe1386bcf9
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-21 00:36:10 +0100

    Update sfx.js

--- greieyn/sfx.js
+++ greieyn/sfx.js
@@ -1,14 +1,34 @@
-function cloieSheean (s) {
-    const context = new AudioContext() || window.AudioContext || window.webkitAudioContext;
+for (let s of Object.entries(sfxThieObj)) {
+    gaarlagheySheeanyn(s);
+}
+
+const sfxCoheksThie = new AudioContext() || new webkitAudioContext();
+const sfxBufferynThie = {};
+
+function gaarlagheySheeanyn (s, veihRaa) {
     const request = new XMLHttpRequest();
-    request.open("GET", s, true);
+    request.open("GET", s[1], true);
     request.responseType = "arraybuffer";
-    request.onload = () => context.decodeAudioData(request.response, onDecoded);
+    request.onload = () => sfxCoheksThie.decodeAudioData(request.response, onDecoded);
     function onDecoded(buffer) {
-        const bufferSource = context.createBufferSource();
+        const bufferSource = sfxCoheksThie.createBufferSource();
         bufferSource.buffer = buffer;
-        bufferSource.connect(context.destination);
-        bufferSource.start();
+        veihRaa ? raaghynBufferyn[s[0]] = bufferSource : sfxBufferynThie[s[0]] = bufferSource;
     };
     request.send();
 };
+
+function cloieSheean (s) {
+    const b =  sfxBufferynThie[s];
+    try {
+        b.start(sfxCoheksThie.currentTime);
+    } catch (marran) {
+        return;
+    }
+    b.connect(sfxCoheksThie.destination);
+    b.onended = () => {
+        b.stop(sfxCoheksThie.currentTime);
+        b.disconnect(sfxCoheksThie.destination);
+        gaarlagheySheeanyn([s, sfxThieObj[s]]);
+    };
+}
