3d48eeba021be00052c5c976556bcf74b21ecb15
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-22 16:08:10 +0100

    Update caghlaaderDuillag.js

--- greieyn/caghlaaderDuillag.js
+++ greieyn/caghlaaderDuillag.js
@@ -1,4 +1,6 @@
 function caghlaaDuillag (t, enmys) {
+    const screenLaadey = document.getElementById("screen-laadey-thie");
+    screenLaadey.style.visibility = "visible";
     localStorage.setItem("trooidGamman", "true");
     localStorage.setItem("kiaullGoll", sessionStorage.getItem("kiaullGoll"));
     localStorage.setItem("sfxGoll", sessionStorage.getItem("sfxGoll"));
