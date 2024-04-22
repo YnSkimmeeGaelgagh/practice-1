bccd3b9d2acd54cbc1b980210c2acf44bd40bfe1
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2024-04-22 16:11:18 +0100

    Update caghlaaderDuillag.js

--- greieyn/caghlaaderDuillag.js
+++ greieyn/caghlaaderDuillag.js
@@ -1,6 +1,8 @@
 function caghlaaDuillag (t, enmys) {
-    // const screenLaadey = document.getElementById("screen-laadey-thie");
-    // screenLaadey.style.visibility = "visible";
+    if (!enmys.endsWith("index.html")) {
+        const screenLaadey = document.getElementById("screen-laadey-thie");
+        screenLaadey.style.visibility = "visible";
+    };
     localStorage.setItem("trooidGamman", "true");
     localStorage.setItem("kiaullGoll", sessionStorage.getItem("kiaullGoll"));
     localStorage.setItem("sfxGoll", sessionStorage.getItem("sfxGoll"));
